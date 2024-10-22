2947b86bb8fbf45c0890c372aebfa8b5472784b4
Frankie <frankiejosefej@gmail.com>
2024-10-22 14:25:36 -0400

    Started implementing header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import React from "react";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">
-        Welcome to MyShowcase!
-      </h1>
-    </div>
+        <Header />
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <div>MyShowcase</div>
+            <nav>
+                <ul>
+                    <li><a>About</a></li>
+                    <li><a>Discover</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
