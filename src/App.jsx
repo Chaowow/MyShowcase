c5408a6ef693fd8450aab3e2b031c9789df8a2f3
Frankie <frankiejosefej@gmail.com>
2024-10-24 14:28:28 -0400

    Continuing header and created files for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes 
+} from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 function App() {
   return (
-        <Header />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const isAuth = false;
+
   return (
-    <header>
-        <div>
-            <div>MyShowcase</div>
-            <nav>
-                <ul>
-                    <li><a>About</a></li>
-                    <li><a>Discover</a></li>
-                </ul>
-            </nav>
-        </div>
+    <header className='bg-indigo-700 shadow-lg p-5'>
+        <div className='container mx-auto flex 
+        justify-between items-center'>
+           <div className=
+           'text-4xl font-bold font-mono'>
+            My Showcase
+            {/* Change font later */}
+            </div>
+
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='discover'>Discover</Link></li>
+
+        
+            {isAuth && (
+              <li><Link to='profile'>Profile</Link></li>
+            )}
+          </ul>
+        </nav>
+
+
+      </div>
     </header>
   )
 }
