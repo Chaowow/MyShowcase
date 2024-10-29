0cb8f493e7e06dc41ab79bd496d9d5315f889d8b
Frankie <frankiejosefej@gmail.com>
2024-10-29 14:31:04 -0400

    Started Home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,28 +9,72 @@ function Header() {
         <div className='container mx-auto flex 
         justify-between items-center'>
            <div className=
-           'text-4xl font-bold font-mono'>
+           'text-6xl font-bold font-mono text-center md:text-left'>
             My Showcase
             {/* Change font later */}
             </div>
+        <div className='flex items-center space-x-4'>
+          <nav>
+            <ul className='flex space-x-4'>
+              <li>
+                <Link 
+                to='/' 
+                className='text-gray-950 hover:text-stone-300'
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link 
+                to='/about' 
+                className='text-gray-950 hover:text-stone-300'
+                >
+                    About
+                </Link>
+              </li>
+              <li>
+                <Link 
+                to='discover' 
+                className='text-gray-950 hover:text-stone-300'
+                >
+                  Discover
+                </Link>
+              </li>
 
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/about'>About</Link></li>
-            <li><Link to='discover'>Discover</Link></li>
+              {/* Conditional Profile link render for now */}
+              {isAuth && (
+                <li>
+                  <Link 
+                  to='profile' 
+                  className='text-gray-950 hover:text-stone-300'
+                  >
+                    Profile
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </nav>
 
-        
-            {isAuth && (
-              <li><Link to='profile'>Profile</Link></li>
-            )}
-          </ul>
-        </nav>
-
-
+        {/* Temporary Sign In/Sign Out Buttons */}
+          {isAuth ? (
+            <button 
+            className='bg-indigo-500 text-white
+            px-4 py-2 rounded-md hover:bg-indigo-600'
+            >
+              Sign Out
+            </button>
+          ) : (
+            <button 
+            className='bg-indigo-500 text-white
+            px-4 py-2 rounded-md hover:bg-indigo-600'
+            >
+              Sign In
+            </button>
+          )}
+        </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to MyShowcase</h1>
+    <div className='bg-indigo-800 min-h-screen flex flex-col 
+    items-center justify-center text-center p-4'>
+      <h1 className='text-4xl md:text-6xl font-bold text-indigo-950 mb-4'>
+        Welcome to MyShowcase.
+      </h1>
+      <p className='text-lg md:text-2xl text-gray-900 max-w-xl mb-8'>
+        Create and track lists of your favorite things, 
+        from movies and TV shows to books and video games. Share with your friends.
+      </p>
+
+      <Link to='/discover'>
+        <button className='bg-indigo-500 text-white px-6 py-3 
+        rounded-lg text-lg md:text-xl hover:bg-indigo-600'>
+          Get Started!
+        </button>
+      </Link>
     </div>
   );
 }
