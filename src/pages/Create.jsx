7e07f8298b53b1d9ebc7cc17dbc8b2f4141d71fc
Frankie <frankiejosefej@gmail.com>
2024-11-09 01:41:33 -0500

    Implemented and style users being able to create a list by adding a title and description

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,20 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-function Form ({ open }) {
+function Form ({ open, onSave }) {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('List Title:', title);
-        console.log('List Description:', description);
+        onSave({ title, description });
+
+        setTitle('');
+        setDescription('');
     };
 
     return (
             open ? (
             <form onSubmit={handleSubmit} className='p-4 border rounded shadow-lg'>
-
                 <label className='block mb-2 text-white'>List Title</label>
                 <input 
                     type='text'
@@ -47,17 +48,32 @@ function Form ({ open }) {
 
 function Create() {
     const [open, setOpen] = useState(false);
-    const [searchResult, setSearchResult] = useState([]);
     const [userList, setUserList] = useState([]);
 
     const openForm = () => setOpen(!open);
+    const saveList = (list) => {
+        setUserList([...userList, list]);
+    };
 
   return (
-    <div className='bg-indigo-950'>
-        <button className='bg-indigo-500 text-white mb-4' onClick={openForm}>
+    <div className='bg-indigo-950 p-6 min-h-screen'>
+        <button className='bg-indigo-500 text-white mb-4 px-4 py-2
+        rounded' onClick={openForm}>
             CREATE NEW LIST
         </button>
-        <Form open={open} />
+
+        <Form open={open} onSave={saveList}/>
+
+        <div className='mt-8'>
+            {userList.length > 0 && 
+            <h3 className='text-2xl text-white mb-4'>Your Lists:</h3>}
+            {userList.map((list, index) => (
+                <div key={index} className='bg-slate-200 p-4 mb-4 rounded shadow'>
+                    <h4 className='text-xl font-semibold'>{list.title}</h4>
+                    <p>{list.description}</p>
+                </div>
+            ))}
+        </div>
     </div>
   );
 }
